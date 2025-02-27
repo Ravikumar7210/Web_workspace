49febf0bdb5058259537203d4a648551bbe5be59
Ravikumar7210 <rk76912@gmail.com>
2025-02-27 18:53:01 +0530

    reduce, every  method practice question is completed

---
+++ DevDSA_apnacollege/JS_Practice/A39.js
@@ -0,0 +1,24 @@
+// let arr = [23,334,45,434,33,34];
+
+// let ans = arr.every ((el) => el %10 ==0);
+
+// console.log(ans)
+
+
+// let nums = [10,20,30,40,5];
+
+// find the min in array using reduce 
+
+let nums = [10,20,30,40,5];
+
+let min = nums.reduce((min ,el ) =>{
+    if (min<el)
+{
+    return min;
+}
+else {
+    return el;
+}
+});
+
+console.log (min);
